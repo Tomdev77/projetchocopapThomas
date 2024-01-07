5ea2052a91ae964368c6f5acf93d2091c701e949
Thom_i <62951436+Tomdev77@users.noreply.github.com>
2024-01-07 17:30:57 +0100

    Update BasketItem.jsx

--- components/BasketItem.jsx
+++ components/BasketItem.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import Badge from "react-bootstrap/Badge";
 
 function BasketItem({ chocolat, totalProduct, handlOpenCart, addItemToCart }) {
-  console.log(totalProduct);
-  return (
-    <div className="basket">
-      <span
-        onClick={() => {
-          handlOpenCart(); // ouverture du panier pop up
-          addItemToCart(chocolat.id); // joue sur l'état du panier pop up, ajoute un produit en fonction de l'id chocolat (contenu)
-        }}
-      >
-        {totalProduct}
-      </span>
-      <img id="panier" src={require("../images/panier.png")} alt="panier" />
-    </div>
-  );
+console.log(totalProduct);
+return (
+<div className="basket">
+<span
+onClick={() => {
+handlOpenCart(); // ouverture du panier pop up
+addItemToCart(chocolat.id); // joue sur l'état du panier pop up, ajoute un produit en fonction de l'id chocolat (contenu)
+}}
+>
+{totalProduct}
+</span>
+<img id="panier" src={require("../images/panier.png")} alt="panier" />
+</div>
+);
 }
 export default BasketItem;
