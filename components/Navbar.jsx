3f63b4aa8c095a8e85cf6f61456855efdcac3ac8
Thom_i <62951436+Tomdev77@users.noreply.github.com>
2024-01-07 17:31:17 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,83 +7,83 @@ import PanierContext from "../store/panier-context";
 // import "../styles/Bootstrap.css";
 
 function Navbar() {
-  const { totalProduct } = useContext(PanierContext);
-  const [showLinks, setshowLinks] = useState(false); // menu fermer par défault
-  const [color, setColor] = useState(false); // changer couleur au scroll de la navbar
-  const [cart, setCart] = useState({});
-  const [chocolat, setChocolats] = useState([]);
-  const handleShowLinks = () => {
-    setshowLinks(!showLinks); // activation du sholinks si true sinon false
-  };
-  const { addItemToCart } = useContext(PanierContext);
+const { totalProduct } = useContext(PanierContext);
+const [showLinks, setshowLinks] = useState(false); // menu fermer par défault
+const [color, setColor] = useState(false); // changer couleur au scroll de la navbar
+const [cart, setCart] = useState({});
+const [chocolat, setChocolats] = useState([]);
+const handleShowLinks = () => {
+setshowLinks(!showLinks); // activation du sholinks si true sinon false
+};
+const { addItemToCart } = useContext(PanierContext);
 
-  const modalRef = useRef();
-  console.log(totalProduct);
-  const handlOpenCart = () => {
-    modalRef.current.open();
-  };
+const modalRef = useRef();
+console.log(totalProduct);
+const handlOpenCart = () => {
+modalRef.current.open();
+};
 
-  // changer couleur au scroll de la navbar
+// changer couleur au scroll de la navbar
 
-  const changeColor = () => {
-    setColor(window.scrollY >= 90);
-  };
+const changeColor = () => {
+setColor(window.scrollY >= 90);
+};
 
-  useEffect(() => {
-    window.addEventListener("scroll", changeColor);
-    return () => {
-      window.removeEventListener("scroll", changeColor);
-    };
-  }, []);
+useEffect(() => {
+window.addEventListener("scroll", changeColor);
+return () => {
+  window.removeEventListener("scroll", changeColor);
+};
+}, []);
 
-  return (
-    <>
-      <CartModal ref={modalRef} />
-      <nav className={color ? "fixed navbar navbar-bg" : "fixed navbar"}>
-        <nav className={`fixed navbar ${showLinks ? "show-nav" : "hide-nav"} `}>
-          <div className="navbar-logo">
-            <img
-              id="logomenucoverdesktop"
-              src={require("../images/logo.png")}
-              alt="logomobilecover"
-            />
-          </div>
+return (
+<>
+  <CartModal ref={modalRef} />
+  <nav className={color ? "fixed navbar navbar-bg" : "fixed navbar"}>
+    <nav className={`fixed navbar ${showLinks ? "show-nav" : "hide-nav"} `}>
+      <div className="navbar-logo">
+        <img
+          id="logomenucoverdesktop"
+          src={require("../images/logo.png")}
+          alt="logomobilecover"
+        />
+      </div>
 
-          <BasketItem
-            chocolat={chocolat}
-            totalProduct={totalProduct}
-            handlOpenCart={handlOpenCart}
-            addItemToCart={addItemToCart}
+      <BasketItem
+        chocolat={chocolat}
+        totalProduct={totalProduct}
+        handlOpenCart={handlOpenCart}
+        addItemToCart={addItemToCart}
+      />
+
+      <ul className="nav-links">
+        <li className="navbar_item ">
+          <a href="/" className="navbar_link">
+            Accueil
+          </a>
+        </li>
+        <li className="navbar-item ">
+          <a href="/boutique" className="navbar_link">
+            Boutique
+          </a>
+        </li>
+
+        <li className="navbar-item ">
+          <img
+            id="logomenucovermobile"
+            src={require("../images/logo.png")}
+            alt="logomobilecover"
           />
+        </li>
 
-          <ul className="nav-links">
-            <li className="navbar_item ">
-              <a href="/" className="navbar_link">
-                Accueil
-              </a>
-            </li>
-            <li className="navbar-item ">
-              <a href="/boutique" className="navbar_link">
-                Boutique
-              </a>
-            </li>
-
-            <li className="navbar-item ">
-              <img
-                id="logomenucovermobile"
-                src={require("../images/logo.png")}
-                alt="logomobilecover"
-              />
-            </li>
-
-            <li className="navbar-item"></li>
-          </ul>
-          <button className="navbar-burger"></button>
-          <div className="burger-bar" onClick={handleShowLinks}></div>
-        </nav>
-      </nav>
-    </>
-  );
+        <li className="navbar-item"></li>
+      </ul>
+      <button className="navbar-burger"></button>
+      <div className="burger-bar" onClick={handleShowLinks}></div>
+    </nav>
+  </nav>
+</>
+);
 }
 
 export default Navbar;
